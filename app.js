7b879117776b09e2a28683ee98919523d3cc1295
Alex Walters <alexnwalters@gmail.com>
2019-05-28 01:47:30 -0400

    create app.js and complete drills

---
+++ app.js
@@ -0,0 +1,175 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('Hello Express!');
+});
+
+app.listen(8000, () => {
+    console.log('Express Server is listening on port 8000!')
+});
+
+app.get('/burgers', (req, res) => {
+    res.send('We have juicy cheese burgers!');
+});
+
+app.get('/pizza/pepperoni', (req, res) => {
+    res.send('Your pizza is on the way!')
+});
+
+app.get('/pizza/pineapple', (req, res) => {
+    res.send('We don\'t serve that here. Never call again!')
+});
+
+app.get('/echo', (req, res) => {
+    const responseText = `Here are some details of your request:
+      Base URL: ${req.baseUrl}
+      Host: ${req.hostname}
+      Path: ${req.path}
+    `;
+    res.send(responseText);
+});
+
+app.get('/queryViewer', (req, res) => {
+console.log(req.query);
+res.end(); //do not send any data back to the client
+});
+
+app.get('/greetings', (req, res) => {
+    //1. get values from the request
+    const name = req.query.name;
+    const race = req.query.race;
+  
+    //2. validate the values
+    if(!name) {
+      //3. name was not provided
+      return res.status(400).send('Please provide a name');
+    }
+  
+    if(!race) {
+      //3. race was not provided
+      return res.status(400).send('Please provide a race');
+    }
+  
+    //4. and 5. both name and race are valid so do the processing.
+    const greeting = `Greetings ${name} the ${race}, welcome to our Kingdom!`;
+  
+    //6. send the response 
+    res.send(greeting);
+});
+
+app.get('/sum', (req, res) => {
+    const a = req.query.a;
+    const b = req.query.b;
+
+    if(!a) {
+        return res.status(400).send('Please provide a Number for A')
+    };
+
+    if(!b) {
+        return res.status(400).send('Please provide a Number for B')
+    };
+
+    const c = `Your sum is ${Number(a) + Number(b)}`;
+
+    res.send(c);
+})
+
+app.get('/cipher', (req, res) => {
+    const text = req.query.text;
+    const shift = req.query.shift;
+
+    const numShift = parseFloat(shift);
+
+    if(!text) {
+        return res.status(400).send('Please provide a string of text');
+    }
+
+    if(!shift) {
+        return res.status(400).send('Please provide a number for shift');
+    }
+
+    const base = 'A'.charCodeAt(0);
+
+    const chipher = text
+        .toUpperCase()
+        .split("")
+        .map(char => {
+            const code = char.charCodeAt(0);
+
+            if(code < base || code > (base + 26)) {
+                return char;
+            }
+
+            let diff = code - base;
+            diff = diff + numShift;
+
+            diff = diff % 26;
+
+            const newLetter = String.fromCharCode(base + diff);
+
+            return newLetter;
+            
+        })
+        .join("");
+
+    res.send(chipher)
+})
+
+app.get('/lotto', (req, res) => {
+    const { numbers } = req.query;
+
+    if(!numbers) {
+        return res
+        .status(400)
+        .send('Please provide query')
+    }
+
+    if(!Array.isArray(numbers)) {
+        return res
+        .status(400)
+        .send('numbers must be an array')
+    }
+
+    const guesses = numbers
+        .map(n => parseInt(n))
+        .filter(n => !Number.isNaN(n) && (n >= 1 && n <= 20));
+    
+    if(guesses.length != 6) {
+        return res
+        .status(400)
+        .send("numbers must contain 6 integers between 1 and 20");
+    }
+    
+    const stockNumbers = Array(20).fill(1).map((_, i) => i + 1);
+
+    const winningNumbers = [];
+    for(let i = 0; i < 6; i++) {
+        const ran = Math.floor(Math.random() * stockNumbers.length);
+        winningNumbers.push(stockNumbers[ran]);
+        stockNumbers.splice(ran, 1);
+    }
+
+    let diff = winningNumbers.filter(n => !guesses.includes(n));
+
+    let responseText;
+
+    switch(diff.length){
+        case 0: 
+        responseText = 'Wow! Unbelievable! You could have won the mega millions!';
+        break;
+        case 1:   
+        responseText = 'Congratulations! You win $100!';
+        break;
+        case 2:
+        responseText = 'Congratulations, you win a free ticket!';
+        break;
+        default:
+        responseText = 'Sorry, you lose';  
+    }
+
+    res.send(responseText);
+})
